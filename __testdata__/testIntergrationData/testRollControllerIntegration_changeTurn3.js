d3594305afde282797a32bccaaba78451acd1646
John <john.seymour787@gmail.com>
2021-05-07 20:16:39 +1000

    Added test 13 for RollController integration tests

---
+++ __testdata__/testIntergrationData/testRollControllerIntegration_changeTurn3.js
@@ -0,0 +1,81 @@
+export default 
+[
+  //Initial state
+  {
+    isManager: false,
+    currentLane: 0,
+    lanes: 
+    [
+      {
+        laneid: "1",
+        laneTitle: "Lane 1",
+        started: true,
+        players: [
+        {
+          playerName: 'User 1', 
+          frames: [[2,4],[5,3],[4,2]], 
+          strike: [{isStrike: false},{isStrike: false},{isStrike: false}], 
+          spare: [{isSpare: false},{isSpare: false},{isSpare: false}], 
+          cumulativeScores: [6, 8, 6],
+          currentRoll: 1,     //Starts at 1
+          maxRolls: 2,
+          lastScore: 0,
+        },
+        {
+          playerName: 'User 2',
+          frames: [[1,1],[1,1],[1,1]],
+          strike: [{isStrike: false},{isStrike: false},{isStrike: false}],
+          spare: [{isSpare: false},{isSpare: false},{isSpare: false}],
+          cumulativeScores: [2, 2, 2],
+          currentRoll: 1,
+          maxRolls: 2,
+          lastScore: 0,
+        }
+        ],
+        currentPlayer: 0,
+        currentFrame: 3,
+        winner: "",
+        ended: false
+      }
+    ]
+  },
+  //Expected result
+  {
+    isManager: false,
+    currentLane: 0,
+    lanes: 
+    [
+      {
+        laneid: "1",
+        laneTitle: "Lane 1",
+        started: true,
+        players: [
+        {
+          playerName: 'User 1', 
+          frames: [[2,4],[5,3],[4,2], [3]], 
+          strike: [{isStrike: false},{isStrike: false},{isStrike: false},{isStrike: false}], 
+          spare: [{isSpare: false},{isSpare: false},{isSpare: false}], 
+          cumulativeScores: [6, 8, 6, 3],
+          currentRoll: 2,     //Changes to 2
+          maxRolls: 2,
+          lastScore: 3,
+        },
+        {
+          playerName: 'User 2',
+          frames: [[1,1],[1,1],[1,1]],
+          strike: [{isStrike: false},{isStrike: false},{isStrike: false}],
+          spare: [{isSpare: false},{isSpare: false},{isSpare: false}],
+          cumulativeScores: [2, 2, 2],
+          currentRoll: 1,
+          maxRolls: 2,
+          lastScore: 0,
+        }
+        ],
+        currentPlayer: 0,
+        currentFrame: 3,
+        winner: "",
+        ended: false
+      }
+    ]
+  }
+];
--- __tests__/johnS/IntegrationChangingTurns.test.js
+++ __tests__/johnS/IntegrationChangingTurns.test.js
@@ -51,3 +51,49 @@ it('11 - Player scores a strike in the non-final frame', () =>
 
     expect(reducerState).toEqual(expectedState);
 })
+
+it('12 - Players scores a normal score in a non-final frame', () => 
+{
+    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn3').default;
+    const currentState = testData[0];
+    const expectedState = testData[1];
+
+    //Create a store outside to track its data later
+    let testStore = createStore(reducer, currentState);
+
+    //Render the integration under test
+    const app = render(<RollController />, { store: testStore });
+
+    //Press a button as part of this test
+    const getById = queryByAttribute.bind(null, 'id');
+    const button3 = getById(app.container, 'pin3');
+    fireEvent.click(button3);
+
+    //Get the updated reducer data
+    const reducerState = testStore.getState();
+
+    expect(reducerState).toEqual(expectedState);
+})
+
+it('13 - Non-final player in last frame on last roll', () => 
+{
+    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn3').default;
+    const currentState = testData[0];
+    const expectedState = testData[1];
+
+    //Create a store outside to track its data later
+    let testStore = createStore(reducer, currentState);
+
+    //Render the integration under test
+    const app = render(<RollController />, { store: testStore });
+
+    //Press a button as part of this test
+    const getById = queryByAttribute.bind(null, 'id');
+    const button3 = getById(app.container, 'pin3');
+    fireEvent.click(button3);
+
+    //Get the updated reducer data
+    const reducerState = testStore.getState();
+
+    expect(reducerState).toEqual(expectedState);
+})
