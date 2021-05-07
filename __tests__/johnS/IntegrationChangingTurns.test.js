db65efdfc9bb32efe8aa3575e7e4bcd3b830cc0d
John <john.seymour787@gmail.com>
2021-05-07 20:47:11 +1000

    Cleaned up integration test code

--- __tests__/johnS/IntegrationChangingTurns.test.js
+++ __tests__/johnS/IntegrationChangingTurns.test.js
@@ -6,9 +6,10 @@ import RollController from '../../src/components/RollController/RollController';
 import reducer from '../../src/store/reducer'
 import { createStore } from 'redux'
 
-it('10 - Last frame with only 2 rolls and 2 strikes => Next player', () => 
+function executeSingleTest(testFileNumber, buttonIDToClick)
 {
-    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn1').default;
+    const fileName = "../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn" + String(testFileNumber);
+    const testData = require(fileName).default;
     const currentState = testData[0];
     const expectedState = testData[1];
 
@@ -20,126 +21,41 @@ it('10 - Last frame with only 2 rolls and 2 strikes => Next player', () =>
 
     //Press a button as part of this test
     const getById = queryByAttribute.bind(null, 'id');
-    const button2 = getById(app.container, 'pin2');
-    fireEvent.click(button2);
+    const buttonToClick = getById(app.container, buttonIDToClick);
+    fireEvent.click(buttonToClick);
 
     //Get the updated reducer data
     const reducerState = testStore.getState();
 
     expect(reducerState).toEqual(expectedState);
+}
+
+it('10 - Last frame with only 2 rolls and 2 strikes => Next player', () => 
+{
+    executeSingleTest(1, "pin2");
 })
 
 it('11 - Player scores a strike in the non-final frame => Next player', () => 
 {
-    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn2').default;
-    const currentState = testData[0];
-    const expectedState = testData[1];
-
-    //Create a store outside to track its data later
-    let testStore = createStore(reducer, currentState);
-
-    //Render the integration under test
-    const app = render(<RollController />, { store: testStore });
-
-    //Press a button as part of this test
-    const getById = queryByAttribute.bind(null, 'id');
-    const button10 = getById(app.container, 'pin10');
-    fireEvent.click(button10);
-
-    //Get the updated reducer data
-    const reducerState = testStore.getState();
-
-    expect(reducerState).toEqual(expectedState);
+    executeSingleTest(2, "pin10");
 })
 
 it('12 - Players scores a normal score in a non-final frame => Next roll', () => 
 {
-    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn3').default;
-    const currentState = testData[0];
-    const expectedState = testData[1];
-
-    //Create a store outside to track its data later
-    let testStore = createStore(reducer, currentState);
-
-    //Render the integration under test
-    const app = render(<RollController />, { store: testStore });
-
-    //Press a button as part of this test
-    const getById = queryByAttribute.bind(null, 'id');
-    const button3 = getById(app.container, 'pin3');
-    fireEvent.click(button3);
-
-    //Get the updated reducer data
-    const reducerState = testStore.getState();
-
-    expect(reducerState).toEqual(expectedState);
+    executeSingleTest(3, "pin3");
 })
 
 it('13 - Non-final player in non-final frame on last roll => Next player', () => 
 {
-    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn4').default;
-    const currentState = testData[0];
-    const expectedState = testData[1];
-
-    //Create a store outside to track its data later
-    let testStore = createStore(reducer, currentState);
-
-    //Render the integration under test
-    const app = render(<RollController />, { store: testStore });
-
-    //Press a button as part of this test
-    const getById = queryByAttribute.bind(null, 'id');
-    const button6 = getById(app.container, 'pin6');
-    fireEvent.click(button6);
-
-    //Get the updated reducer data
-    const reducerState = testStore.getState();
-
-    expect(reducerState).toEqual(expectedState);
+    executeSingleTest(4, "pin6");
 })
 
 it('14 - Non-final player in last frame on last roll => Next player', () => 
 {
-    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn5').default;
-    const currentState = testData[0];
-    const expectedState = testData[1];
-
-    //Create a store outside to track its data later
-    let testStore = createStore(reducer, currentState);
-
-    //Render the integration under test
-    const app = render(<RollController />, { store: testStore });
-
-    //Press a button as part of this test
-    const getById = queryByAttribute.bind(null, 'id');
-    const button3 = getById(app.container, 'pin3');
-    fireEvent.click(button3);
-
-    //Get the updated reducer data
-    const reducerState = testStore.getState();
-
-    expect(reducerState).toEqual(expectedState);
+    executeSingleTest(5, "pin3");
 })
 
 it('15 - Non-final player in last frame on last roll => Next roll', () => 
 {
-    const testData = require('../../__testdata__/testIntergrationData/testRollControllerIntegration_changeTurn6').default;
-    const currentState = testData[0];
-    const expectedState = testData[1];
-
-    //Create a store outside to track its data later
-    let testStore = createStore(reducer, currentState);
-
-    //Render the integration under test
-    const app = render(<RollController />, { store: testStore });
-
-    //Press a button as part of this test
-    const getById = queryByAttribute.bind(null, 'id');
-    const button7 = getById(app.container, 'pin7');
-    fireEvent.click(button7);
-
-    //Get the updated reducer data
-    const reducerState = testStore.getState();
-
-    expect(reducerState).toEqual(expectedState);
+    executeSingleTest(6, "pin7");
 })
