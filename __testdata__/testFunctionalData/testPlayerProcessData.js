6cba738b41fb512414fbc176d7faadfbbf65b1ef
John <john.seymour787@gmail.com>
2021-05-08 13:11:33 +1000

    Added PlayerProcessFunctional test 2, to be cleaned up later

--- __testdata__/testFunctionalData/testPlayerProcessData.js
+++ __testdata__/testFunctionalData/testPlayerProcessData.js
@@ -43,5 +43,50 @@ module.exports =
                 },
             ]
         }
-    }   
+    },
+
+    test2Data:
+    {
+        buttonIDToClick: "pin8",
+        expectedScore: "8",
+        reducerState:
+        {
+            isManager: false,
+            currentLane: 0,
+            lanes: 
+            [
+                {
+                    laneid: "1",
+                    laneTitle: "Lane 1",
+                    started: true,
+                    players: [
+                        {
+                            playerName: 'User 1',
+                            frames: [[1,2],[4,2],[4,5],[6,4],[4,4]],
+                            strike: [{isStrike: false},{isStrike: false},{isStrike: false},{isStrike: false},{isStrike: false}],
+                            spare: [{isSpare: false},{isSpare: false},{isSpare: false},{isSpare: true, nextScore: [4]},{isSpare: false}],
+                            cumulativeScores: [3,6,9,10,8],
+                            currentRoll: 1,
+                            maxRolls: 2,
+                            lastScore: 0,
+                        },
+                        {
+                            playerName: 'User 2',
+                            frames: [[1,5],[7,2],[1,4],[5,4]],
+                            strike: [{isStrike: false},{isStrike: false},{isStrike: false},{isStrike: false}],
+                            spare: [{isSpare: false},{isSpare: false},{isSpare: false},{isSpare: false}],
+                            cumulativeScores: [6,9,5,9],
+                            currentRoll: 1,
+                            maxRolls: 2,
+                            lastScore: 0,
+                        },
+                    ],
+                    currentPlayer: 1,       //Player 2
+                    currentFrame: 4,        //Frame 5
+                    winner: "",
+                    ended: false
+                },
+            ]
+        }
+    }
 };
--- __tests__/johnS/FunctionalPlayerProcess.test.js
+++ __tests__/johnS/FunctionalPlayerProcess.test.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import { render, fireEvent } from '../../__utils__/test-utils';
 import { queryByAttribute } from '@testing-library/react';
-import { test1Data } from '../../__testdata__/testFunctionalData/testPlayerProcessData';
+import { test1Data, test2Data } from '../../__testdata__/testFunctionalData/testPlayerProcessData';
 import App from '../../src/containers/App';
 
 
@@ -12,7 +12,6 @@ it('1 - Player enters first score for first roll => ScoreBoard, RollController, 
     const originalError = console.error;
     console.error = jest.fn();
 
-    // const testData = 
 
     //Render the entire application
     const app = render(<App />, { initialState: test1Data.reducerState });
@@ -79,6 +78,94 @@ it('1 - Player enters first score for first roll => ScoreBoard, RollController, 
     //Check that the rollstatus displays the updated roll data
     expect(rollStatus.innerHTML).toContain("Current Roll: 2");
 
+//#endregion
+
+
+    //Return any error if there was one originally
+    console.error = originalError;
+})
+
+
+it('2 - Player plays in 5th frame => ScoreBoard, RollController, and RollStatus all update', () => 
+{
+    //Ignore warnings during rendering
+    const originalError = console.error;
+    console.error = jest.fn();
+
+    // const testData = 
+
+    //Render the entire application
+    const app = render(<App />, { initialState: test2Data.reducerState });
+
+    //Player 1 presses button 5 to score
+    const getById = queryByAttribute.bind(null, 'id');
+    const buttonToClick = getById(app.container, test2Data.buttonIDToClick);
+    
+    fireEvent.click(buttonToClick);
+
+//#region Test the ScoreBoard exists and is updated
+
+    //Check that the whole scoreboard exists
+    const scoreboard = getById(app.container, 'scoreboard');
+    const isScoreboardDisplayed = (scoreboard) ? true : false;
+    expect(isScoreboardDisplayed);
+
+    //Check that the individual scoreitem exists that should be updated; ID is "r1" + *PlayerName*
+    const scoreItem = getById(app.container, 'r9' + test2Data.reducerState.lanes[0].players[1].playerName);
+    const isScoreItemDisplayed = (scoreItem) ? true : false;
+    expect(isScoreItemDisplayed);
+    //Check that the scoreitem displays the score just achieved
+    expect(scoreItem.innerHTML).toBe(test2Data.expectedScore);
+
+//#endregion
+
+//#region Test RollController exists and is updated
+
+    //Check that the whole RollController exists
+    const rollController = getById(app.container, 'roll-controller');
+    const isrollControllerDisplayed = (rollController) ? true : false;
+    expect(isrollControllerDisplayed); 
+
+    //Button list in the RollController's children
+    const buttonsList = rollController.children[0].children;
+
+    //Calculate the number of buttons that should be enabled (including button 0 (-))
+    const enabledButtonsCount = 10 - Number(test2Data.expectedScore) + 1;
+    //Check that the correct score buttons are disabled or enabled
+    let i = 0;
+    //Buttons 0 to 5 inclusive should be enabled and without the disabled attribute
+    while (i < enabledButtonsCount)
+    {
+        expect(buttonsList[i].hasAttribute("disabled")).toBe(false);
+        expect(buttonsList[i]).toBeEnabled();
+        i++;
+    }
+    //Buttons 6 to 10 should be disabled, including with the attribute
+    while(i < buttonsList.length)
+    {
+        expect(buttonsList[i].hasAttribute("disabled")).toBe(true);
+        expect(buttonsList[i]).toBeDisabled();
+        i++;
+    }
+//#endregion
+
+//#region RollStatus component checking
+
+    //Check that the RollStatus exists
+    const rollStatus = getById(app.container, 'roll-status');
+    const isrollStatusDisplayed = (rollStatus) ? true : false;
+    expect(isrollStatusDisplayed); 
+
+    //Check that the rollstatus displays the updated roll data
+    expect(rollStatus.innerHTML).toContain("Current Roll: 2");
+
+//#endregion
+
+//#region Test the total score exists and is correctly updated
+    const totalScore = getById(app.container, 'total-score'+test2Data.reducerState.lanes[0].players[1].playerName);
+    const istotalScoreDisplayed = (totalScore) ? true : false;
+    expect(istotalScoreDisplayed); 
+    expect(totalScore.innerHTML).toBe("37");
 //#endregion
 
 
