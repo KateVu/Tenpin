ea76b751b2c431af69de2467de8993190dd57d89
John <john.seymour787@gmail.com>
2021-05-08 16:05:39 +1000

    Cleaned up PlayerProcess functional tests

--- __tests__/johnS/FunctionalPlayerProcess.test.js
+++ __tests__/johnS/FunctionalPlayerProcess.test.js
@@ -13,73 +13,22 @@ it('1 - Player enters first score for first roll => ScoreBoard, RollController, 
     console.error = jest.fn();
 
 
-    //Render the entire application
     const app = render(<App />, { initialState: test1Data.reducerState });
 
-    //Player 1 presses button 5 to score
+    //Player presses button to score
     const getById = queryByAttribute.bind(null, 'id');
     const buttonToClick = getById(app.container, test1Data.buttonIDToClick);
-    
     fireEvent.click(buttonToClick);
 
-//#region Test the ScoreBoard exists and is updated
-
     //Check that the whole scoreboard exists
-    const scoreboard = getById(app.container, 'scoreboard');
-    const isScoreboardDisplayed = (scoreboard) ? true : false;
-    expect(isScoreboardDisplayed);
+    const currentPlayerName = getCurrentPlayerName(test1Data);
+    testScoreBoard(getById, app, "r1", currentPlayerName, test1Data.expectedScore);
 
-    //Check that the individual scoreitem exists that should be updated; ID is "r1" + *PlayerName*
-    const scoreItem = getById(app.container, 'r1' + test1Data.reducerState.lanes[0].players[0].playerName);
-    const isScoreItemDisplayed = (scoreItem) ? true : false;
-    expect(isScoreItemDisplayed);
-    //Check that the scoreitem displays the score just achieved
-    expect(scoreItem.innerHTML).toBe(test1Data.expectedScore);
-
-//#endregion
-
-//#region Test RollController exists and is updated
-
-    //Check that the whole RollController exists
-    const rollController = getById(app.container, 'roll-controller');
-    const isrollControllerDisplayed = (rollController) ? true : false;
-    expect(isrollControllerDisplayed); 
-
-    //Button list in the RollController's children
-    const buttonsList = rollController.children[0].children;
-
-    //Calculate the number of buttons that should be enabled (including button 0 (-))
-    const enabledButtonsCount = 10 - Number(test1Data.expectedScore) + 1;
-    //Check that the correct score buttons are disabled or enabled
-    let i = 0;
-    //Buttons 0 to 5 inclusive should be enabled and without the disabled attribute
-    while (i < enabledButtonsCount)
-    {
-        expect(buttonsList[i].hasAttribute("disabled")).toBe(false);
-        expect(buttonsList[i]).toBeEnabled();
-        i++;
-    }
-    //Buttons 6 to 10 should be disabled, including with the attribute
-    while(i < buttonsList.length)
-    {
-        expect(buttonsList[i].hasAttribute("disabled")).toBe(true);
-        expect(buttonsList[i]).toBeDisabled();
-        i++;
-    }
-//#endregion
-
-//#region RollStatus component checking
+    //Check that the whole RollController exists and was updated
+    testRollControllerUpdated(getById, app, test1Data);
 
     //Check that the RollStatus exists
-    const rollStatus= getById(app.container, 'roll-status');
-    const isrollStatusDisplayed = (rollStatus) ? true : false;
-    expect(isrollStatusDisplayed); 
-
-    //Check that the rollstatus displays the updated roll data
-    expect(rollStatus.innerHTML).toContain("Current Roll: 2");
-
-//#endregion
-
+    testRollStatusUpdated(getById, app);
 
     //Return any error if there was one originally
     console.error = originalError;
@@ -92,80 +41,26 @@ it('2 - Player plays in 5th frame => ScoreBoard, RollController, and RollStatus 
     const originalError = console.error;
     console.error = jest.fn();
 
-
-    //Render the entire application
+    
     const app = render(<App />, { initialState: test2Data.reducerState });
 
-    //Player 1 presses button 5 to score
+    //Player presses button to score
     const getById = queryByAttribute.bind(null, 'id');
     const buttonToClick = getById(app.container, test2Data.buttonIDToClick);
-    
     fireEvent.click(buttonToClick);
 
-//#region Test the ScoreBoard exists and is updated
-
     //Check that the whole scoreboard exists
-    const scoreboard = getById(app.container, 'scoreboard');
-    const isScoreboardDisplayed = (scoreboard) ? true : false;
-    expect(isScoreboardDisplayed);
+    const currentPlayerName = getCurrentPlayerName(test2Data);
+    testScoreBoard(getById, app, "r9", currentPlayerName, test2Data.expectedScore);
 
-    //Check that the individual scoreitem exists that should be updated; ID is "r9" + *PlayerName*
-    const scoreItem = getById(app.container, 'r9' + test2Data.reducerState.lanes[0].players[1].playerName);
-    const isScoreItemDisplayed = (scoreItem) ? true : false;
-    expect(isScoreItemDisplayed);
-    //Check that the scoreitem displays the score just achieved
-    expect(scoreItem.innerHTML).toBe(test2Data.expectedScore);
-
-//#endregion
-
-//#region Test RollController exists and is updated
-
-    //Check that the whole RollController exists
-    const rollController = getById(app.container, 'roll-controller');
-    const isrollControllerDisplayed = (rollController) ? true : false;
-    expect(isrollControllerDisplayed); 
-
-    //Button list in the RollController's children
-    const buttonsList = rollController.children[0].children;
-
-    //Calculate the number of buttons that should be enabled (including button 0 (-))
-    const enabledButtonsCount = 10 - Number(test2Data.expectedScore) + 1;
-    //Check that the correct score buttons are disabled or enabled
-    let i = 0;
-    //Buttons 0 to 5 inclusive should be enabled and without the disabled attribute
-    while (i < enabledButtonsCount)
-    {
-        expect(buttonsList[i].hasAttribute("disabled")).toBe(false);
-        expect(buttonsList[i]).toBeEnabled();
-        i++;
-    }
-    //Buttons 6 to 10 should be disabled, including with the attribute
-    while(i < buttonsList.length)
-    {
-        expect(buttonsList[i].hasAttribute("disabled")).toBe(true);
-        expect(buttonsList[i]).toBeDisabled();
-        i++;
-    }
-//#endregion
-
-//#region RollStatus component checking
+    //Check that the whole RollController exists and was updated
+    testRollControllerUpdated(getById, app, test2Data);
 
     //Check that the RollStatus exists
-    const rollStatus = getById(app.container, 'roll-status');
-    const isrollStatusDisplayed = (rollStatus) ? true : false;
-    expect(isrollStatusDisplayed); 
+    testRollStatusUpdated(getById, app);
 
-    //Check that the rollstatus displays the updated roll data
-    expect(rollStatus.innerHTML).toContain("Current Roll: 2");
-
-//#endregion
-
-//#region Test the total score exists and is correctly updated
-    const totalScore = getById(app.container, 'total-score'+test2Data.reducerState.lanes[0].players[1].playerName);
-    const istotalScoreDisplayed = (totalScore) ? true : false;
-    expect(istotalScoreDisplayed); 
-    expect(totalScore.innerHTML).toBe("37");
-//#endregion
+    //Check that the totoal score is displaying correctly
+    testTotalScore(getById, app, currentPlayerName, "37");
 
 
     //Return any error if there was one originally
@@ -183,27 +78,15 @@ it('3 - Player plays last frame => ScoreBoard updates, RollController and RollSt
     //Render the entire application
     const app = render(<App />, { initialState: test3Data.reducerState });
 
-    //Player 1 presses button 5 to score
+    //Player presses button to score
     const getById = queryByAttribute.bind(null, 'id');
     const buttonToClick = getById(app.container, test3Data.buttonIDToClick);
     
     fireEvent.click(buttonToClick);
 
-//#region Test the ScoreBoard exists and is updated
-
-    //Check that the whole scoreboard exists
-    const scoreboard = getById(app.container, 'scoreboard');
-    const isScoreboardDisplayed = (scoreboard) ? true : false;
-    expect(isScoreboardDisplayed);
-
-    //Check that the individual scoreitem exists that should be updated; ID is "r20" + *PlayerName*
-    const scoreItem = getById(app.container, 'r20' + test3Data.reducerState.lanes[0].players[1].playerName);
-    const isScoreItemDisplayed = (scoreItem) ? true : false;
-    expect(isScoreItemDisplayed);
-    //Check that the scoreitem displays the score just achieved
-    expect(scoreItem.innerHTML).toBe(test3Data.expectedScore);
-
-//#endregion
+    //Test the ScoreBoard exists and is updated
+    const currentPlayerName = getCurrentPlayerName(test3Data);
+    testScoreBoard(getById, app, "r20", currentPlayerName, test3Data.expectedScore)
 
 //#region Test that some components have been removed
 
@@ -219,14 +102,8 @@ it('3 - Player plays last frame => ScoreBoard updates, RollController and RollSt
 
 //#endregion
 
-//#region Test the total score exists and is correctly updated
-
-    const totalScore = getById(app.container, 'total-score'+test3Data.reducerState.lanes[0].players[1].playerName);
-    const istotalScoreDisplayed = (totalScore) ? true : false;
-    expect(istotalScoreDisplayed); 
-    expect(totalScore.innerHTML).toBe("27");
-
-//#endregion
+    //Test the total score exists and is correctly updated
+    testTotalScore(getById, app, currentPlayerName, "27");
 
     //Test that the correct endgame message is displayed
     expect(screen.getByText("The game has ended, the winner is User 2")).toBeInTheDocument();
@@ -272,3 +149,77 @@ it('4 - Player can change a lane => Game updates, different roll status, differe
     //Return any error if there was one originally
     console.error = originalError;
 })
+
+
+//#region Support functions to keep test code cleaner
+
+//Returns the string name of the current player
+function getCurrentPlayerName(lTestData)
+{
+    const currentPlayerIndex = lTestData.reducerState.lanes[lTestData.reducerState.currentLane].currentPlayer;
+    const currentPlayerName = lTestData.reducerState.lanes[lTestData.reducerState.currentLane].players[currentPlayerIndex].playerName;
+    return currentPlayerName;
+}
+
+function testScoreBoard(getById, app, scoreitemRowIDPrefix, currentPlayerName, expectedScore)
+{
+    const scoreboard = getById(app.container, 'scoreboard');
+    const isScoreboardDisplayed = (scoreboard) ? true : false;
+    expect(isScoreboardDisplayed);
+
+    //Check that the individual scoreitem exists that should be updated; eg, ID is "r1" + *PlayerName*
+    const scoreItem = getById(app.container, scoreitemRowIDPrefix + currentPlayerName);
+    const isScoreItemDisplayed = (scoreItem) ? true : false;
+    expect(isScoreItemDisplayed);
+    //Check that the scoreitem displays the score just achieved
+    expect(scoreItem.innerHTML).toBe(expectedScore);
+}
+
+function testRollControllerUpdated(getById, app, lTestData)
+{
+    const rollController = getById(app.container, 'roll-controller');
+    const isrollControllerDisplayed = (rollController) ? true : false;
+    expect(isrollControllerDisplayed);
+
+    //Button list in the RollController's children
+    const buttonsList = rollController.children[0].children;
+
+    //Calculate the number of buttons that should be enabled (including button 0 (-))
+    const enabledButtonsCount = 10 - Number(lTestData.expectedScore) + 1;
+    //Check that the correct score buttons are disabled or enabled
+    let i = 0;
+    //Buttons 0 to enabledButtonsCount inclusive should be enabled and without the disabled attribute
+    while (i < enabledButtonsCount)
+    {
+        expect(buttonsList[i].hasAttribute("disabled")).toBe(false);
+        expect(buttonsList[i]).toBeEnabled();
+        i++;
+    }
+    //Buttons (enabledButtonsCount + 1) to 10 should be disabled and include with the disabled attribute
+    while (i < buttonsList.length)
+    {
+        expect(buttonsList[i].hasAttribute("disabled")).toBe(true);
+        expect(buttonsList[i]).toBeDisabled();
+        i++;
+    }
+}
+
+function testRollStatusUpdated(getById, app)
+{
+    const rollStatus = getById(app.container, 'roll-status');
+    const isrollStatusDisplayed = (rollStatus) ? true : false;
+    expect(isrollStatusDisplayed);
+
+    //Check that the rollstatus displays the updated roll data
+    expect(rollStatus.innerHTML).toContain("Current Roll: 2");
+}
+
+function testTotalScore(getById, app, playerName, expectedTotalScore)
+{
+    const totalScore = getById(app.container, 'total-score' + playerName);
+    const istotalScoreDisplayed = (totalScore) ? true : false;
+    expect(istotalScoreDisplayed);
+    expect(totalScore.innerHTML).toBe(expectedTotalScore);
+}
+
+//#endregion
