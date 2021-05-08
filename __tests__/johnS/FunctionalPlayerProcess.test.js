67c4061e6cdc6acd7fffc52fb1c957be034cd26d
John <john.seymour787@gmail.com>
2021-05-08 12:49:24 +1000

    Updated PlayerProcess functional test 1 to have more dynamic data in the test data

--- __tests__/johnS/FunctionalPlayerProcess.test.js
+++ __tests__/johnS/FunctionalPlayerProcess.test.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import { render, fireEvent } from '../../__utils__/test-utils';
 import { queryByAttribute } from '@testing-library/react';
-import testData from '../../__testdata__/testFunctionalData/testPlayerProcessData';
+import { test1Data } from '../../__testdata__/testFunctionalData/testPlayerProcessData';
 import App from '../../src/containers/App';
 
 
@@ -12,13 +12,14 @@ it('1 - Player enters first score for first roll => ScoreBoard, RollController, 
     const originalError = console.error;
     console.error = jest.fn();
 
+    // const testData = 
 
     //Render the entire application
-    const app = render(<App />, { initialState: testData.test1Data });
+    const app = render(<App />, { initialState: test1Data.reducerState });
 
     //Player 1 presses button 5 to score
     const getById = queryByAttribute.bind(null, 'id');
-    const buttonToClick = getById(app.container, 'pin5');
+    const buttonToClick = getById(app.container, test1Data.buttonIDToClick);
     
     fireEvent.click(buttonToClick);
 
@@ -26,15 +27,15 @@ it('1 - Player enters first score for first roll => ScoreBoard, RollController, 
 
     //Check that the whole scoreboard exists
     const scoreboard = getById(app.container, 'scoreboard');
-    const isScoreboardDisplayed = (scoreboard) ? true : false
+    const isScoreboardDisplayed = (scoreboard) ? true : false;
     expect(isScoreboardDisplayed);
 
-    //Check that the individual scoreitem exists that should be updated
-    const scoreItem = getById(app.container, 'r1User 1');
-    const isScoreItemDisplayed = (scoreItem) ? true : false
+    //Check that the individual scoreitem exists that should be updated; ID is "r1" + *PlayerName*
+    const scoreItem = getById(app.container, 'r1' + test1Data.reducerState.lanes[0].players[0].playerName);
+    const isScoreItemDisplayed = (scoreItem) ? true : false;
     expect(isScoreItemDisplayed);
     //Check that the scoreitem displays the score just achieved
-    expect(scoreItem.innerHTML).toBe('5');
+    expect(scoreItem.innerHTML).toBe(test1Data.expectedScore);
 
 //#endregion
 
@@ -42,15 +43,18 @@ it('1 - Player enters first score for first roll => ScoreBoard, RollController, 
 
     //Check that the whole RollController exists
     const rollController = getById(app.container, 'roll-controller');
-    const isrollControllerDisplayed = (rollController) ? true : false
+    const isrollControllerDisplayed = (rollController) ? true : false;
     expect(isrollControllerDisplayed); 
 
     //Button list in the RollController's children
-    const buttonsList = rollController.children[0].children
+    const buttonsList = rollController.children[0].children;
+
+    //Calculate the number of buttons that should be enabled (including button 0 (-))
+    const enabledButtonsCount = 10 - Number(test1Data.expectedScore) + 1;
     //Check that the correct score buttons are disabled or enabled
     let i = 0;
     //Buttons 0 to 5 inclusive should be enabled and without the disabled attribute
-    while (i < 6)
+    while (i < enabledButtonsCount)
     {
         expect(buttonsList[i].hasAttribute("disabled")).toBe(false);
         expect(buttonsList[i]).toBeEnabled();
@@ -69,13 +73,14 @@ it('1 - Player enters first score for first roll => ScoreBoard, RollController, 
 
     //Check that the RollStatus exists
     const rollStatus= getById(app.container, 'roll-status');
-    const isrollStatusDisplayed = (rollStatus) ? true : false
+    const isrollStatusDisplayed = (rollStatus) ? true : false;
     expect(isrollStatusDisplayed); 
 
     //Check that the rollstatus displays the updated roll data
     expect(rollStatus.innerHTML).toContain("Current Roll: 2");
-    
+
 //#endregion
+
 
     //Return any error if there was one originally
     console.error = originalError;
